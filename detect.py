28203e92210bfda1495eaaf76017be058687c6b0
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-08-13 10:38:20 +0900

    ADD:web cloning test

---
+++ detect.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import commands
+
+from slacker import Slacker
+from selenium import webdriver
+
+
+USERNAME = "<script>alart(1)</script>"
+
+def slack_message_test(username=USERNAME):
+  with open("../credentials/slack_api_key", "r") as f:
+    api_key = f.read()
+    slack = Slacker(api_key)
+    text = "This is test!"
+    # Send a message to #general channel
+    slack.chat.post_message(channel='#detect_log', username=username, text=text)
+
+def web_cloning():
+  default = commands.getoutput("http http://172.17.248.85")
+  print default
+  
+    
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print "Usage: "+sys.argv[0]+" TARGET_URL"
+    exit()
+    
+  #slack_message_test()
+  web_cloning()
+  
